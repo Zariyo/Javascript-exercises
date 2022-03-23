4c580d5bacad8b980c2ef85638d89ea0250407ee
Wiktor Płużek <w.pluzek.409@studms.ug.edu.pl>
2022-03-23 11:57:55 +0000

    Upload New File

---
+++ lab3/zad0.js
@@ -0,0 +1,16 @@
+
+function funkcja1(){
+    console.log("Funkcja 1");
+};
+
+const funkcja2 = () =>{
+    console.log('Funkcja 2');
+};
+
+const funkcja3 = function() {
+    console.log("Funkcja 3");
+};
+
+funkcja1();
+funkcja2();
+funkcja3();
